801468f2c13570e9abd73a6426c7c733950eccb1
BNOK <nabli.hamza112@gmail.com>
2025-11-23 23:04:33 +0000

    updated the way the file is read, set up globals to hold the necessary data then to be drawn. spawn the shapes and update their position inside the game loop to initiate the animation process. try to update the code in a way to be more efficient.

--- main.cpp
+++ main.cpp
@@ -9,188 +9,163 @@
   private variable naming : dashed camel case (Example : int _maxPlayerHealth)
   global variables : PascalCase (Example : int MaxPlayerHealth)*/
 
-// simple shapes class
-class SimpleShapes {
-public:
-    std::string Name = "";
-    std::shared_ptr<sf::Vector2<float>> ShapePosition = std::make_shared<sf::Vector2<float>>(0.0f,0.0f);
-    std::shared_ptr<sf::Vector2<float>> ShapeSpeed = std::make_shared<sf::Vector2<float>>(0.0f,0.0f);
-    std::shared_ptr<sf::Color> ShapeColor = std::make_shared<sf::Color>(255,255,255);
-
-    SimpleShapes(const std::string& name, std::shared_ptr<sf::Vector2<float>> position, std::shared_ptr<sf::Vector2<float>> speed, std::shared_ptr<sf::Color> color)
-        :Name(name), ShapePosition(position), ShapeSpeed(speed), ShapeColor(color) { }
-
-    virtual sf::Shape* getShape() {
-        return nullptr;
-    }
+struct Circle {
+    std::string name;
+    sf::Vector2<float> position = sf::Vector2(100.0f, 100.0f);
+    sf::Vector2<float> speed = sf::Vector2( 0.1f, 0.1f);
+    sf::Color color = sf::Color::White;
+    int radius = 10;
 };
 
-class SimpleCircle : public SimpleShapes {
-
-    sf::CircleShape Shape;
-
-public :
-
-    int Radius;
-    // default constructor with sensible defaults
-    SimpleCircle()
-        : SimpleShapes(std::string("Circle"),
-            std::make_shared<sf::Vector2<float>>(0, 0),
-            std::make_shared<sf::Vector2<float>>(0, 0),
-            std::make_shared<sf::Color>(255, 255, 255)),
-        Radius(10) 
-    {
-        Shape.setFillColor(*ShapeColor);
-        Shape.setPosition(*ShapePosition);
-    }
-
-    // parameterized constructor for the simple circle
-    SimpleCircle(std::string& name, int radius, std::shared_ptr<sf::Vector2<float>> position, std::shared_ptr<sf::Vector2<float>> speed, std::shared_ptr<sf::Color> color)
-        :SimpleShapes(name,position,speed,color), Radius(radius)
-    {
-        Shape.setFillColor(*ShapeColor);
-        Shape.setPosition(*ShapePosition);
-    }
-
-    sf::Shape* getShape() override {
-        std::cout << "get the circle shape !" << std::endl;
-        return &Shape;
-    }
-};
-
-class SimpleRectangle : public SimpleShapes {
-
-    sf::RectangleShape rectangle;
-public :
-    std::shared_ptr<sf::Vector2<int>> Size;
-    SimpleRectangle()
-        : SimpleShapes(std::string("Circle"),
-            std::make_shared<sf::Vector2<float>>(0, 0),
-            std::make_shared<sf::Vector2<float>>(0, 0),
-            std::make_shared<sf::Color>(255, 255, 255)),
-        Size(std::make_shared<sf::Vector2<int>>(10, 10)) {
-    }
-    SimpleRectangle(const std::string& name, std::shared_ptr<sf::Vector2<int>> size, std::shared_ptr<sf::Vector2<float>> position, std::shared_ptr<sf::Vector2<float>> speed, std::shared_ptr<sf::Color> color)
-        :SimpleShapes(name, position, speed, color),Size(size){ }
+struct Rectangle {
+    std::string name;
+    sf::Vector2<float> position = sf::Vector2(100.0f, 100.0f);
+    sf::Vector2<float> speed = sf::Vector2(0.1f, 0.1f);
+    sf::Color color = sf::Color::White;
+    sf::Vector2<float> Size = sf::Vector2<float>(50.0f, 50.0f);
 };
 
 class ConfigFileReader {
-    std::ifstream configFile;
-    int windowSize[2] = {0,0};
-
-    std::vector<SimpleShapes*> shapeHolder;
-
+    std::ifstream inputFile;
 public:
     ConfigFileReader() = default;
+    
 
-    ConfigFileReader(std::string Path = "") {
-        if (Path.empty()) {
-            std::cout << "String is empty (invalid)." << std::endl;
-        }
-        else
-        {
-            configFile.open(Path);
-            if (!configFile.is_open()) {
-                std::cerr << "Error opening file!" << std::endl;
-            }
-            else
-            {
-                std::cout << "File opened successfully !" << std::endl;
-                //getWindowConfig(configFile);
-                
-            }
-        }
-    }
+    ConfigFileReader(const std::string& path, sf::Vector2u& winSize, sf::Font& font, std::vector<Circle>& circles, std::vector<Rectangle>& rectangles)
+    {
+        inputFile;
+        inputFile.open(path);
+        std::cout << "File opened !"<<std::endl;
 
-    ~ConfigFileReader() {
-        if (configFile.is_open()) {
-            configFile.close(); 
-            std::cout << "File object closed !";
-        }
-    }
+        if (inputFile.is_open()) {
 
-    std::vector<SimpleShapes> getShapes() {
-        return shapeHolder;
-    }
+            std::string line;
 
-    std::vector<std::string> split_sentence(std::string sen) {
+            while (std::getline(inputFile, line)) {
+                std::stringstream ss(line);
+                std::string word;
 
-        // Create a stringstream object
-        std::stringstream ss(sen);
-
-        // Variable to hold each word
-        std::string  word;
-
-        // Vector to store the words
-        std::vector<std::string> words;
-
-        // Extract words from the sentence
-        while (ss >> word) {
-
-            // Add the word to the vector
-            words.push_back(word);
-        }
-
-        return words;
-    }
-
-    void getWindowConfig() {
-        std::string line;
-        
-        while (std::getline(configFile, line)) {
-            // Process string Line
-            std::vector<std::string> result = split_sentence(line);
-            if (result.size() != 0)
-            {
-                if (!result[0].compare("Window")) {
-                    windowSize[0] = std::stoi(result[1]);
-                    windowSize[1] = std::stoi(result[2]);
-
-                    std::cout << "Window size set !" << std::endl;
-                    
+                (ss >> word);
+                if (word.length() == 0) {
+                    continue;
                 }
-                else if(!result[0].compare("Font"))
-                {
-                    std::cout << "Found a Font !" << std::endl;
-                }
-                else if (!result[0].compare("Circle"))
-                {
-                    std::cout << "Found a circle !" << std::endl;
-                    // Example: Circle name posX posY speedX speedY colorR colorG colorB radius
-                    if (result.size() >= 9) {
-                        std::string name = result[1];
-                        int radius = std::stoi(result[9]);
-                        std::shared_ptr<sf::Vector2<float>> position = std::make_shared<sf::Vector2<float>>(std::stoi(result[2]), std::stoi(result[3]));
-                        std::shared_ptr<sf::Vector2<float>> speed = std::make_shared<sf::Vector2<float>>(std::stoi(result[4]), std::stoi(result[5]));
-                        std::shared_ptr<sf::Color> color = std::make_shared<sf::Color>(std::stoi(result[6]), std::stoi(result[7]), std::stoi(result[8]));
+                else {
+                    /*std::cout << word << std::endl;
+                    while (ss >> word) {
+                        std::cout << "value is : " << word << std::endl;
+                    }*/
 
-                        SimpleCircle* tempCircle(name, radius, position, speed, color);
-
-                        shapeHolder.push_back(tempCircle);
+                    std::vector<std::string> params = SplitSentence(line);
+                    std::cout << " size : " << params.size() << std::endl;
+                    if (params[0] == "Window" && params.size() == 3) {
+                        std::cout << "found window" << std::endl;
+                        winSize.x = std::stoi(params[1]);
+                        winSize.y = std::stoi(params[2]);
+                    }
+                    else if (params[0] == "Font" && params.size() == 6) {
+                        // set font later
+                        std::cout << "found font" << std::endl;
+                    }
+                    else if (params[0] == "Circle" && params.size() == 10) {
+                        std::cout << "found circle !" << std::endl;
+                        Circle c;
+                        c.name = params[1];
+                        c.position.x = std::stof(params[2]);
+                        c.position.y = std::stof(params[3]);
+                        c.speed.x = std::stof(params[4]);
+                        c.speed.y = std::stof(params[5]);
+                        c.color = sf::Color(std::stoi(params[6]), std::stoi(params[7]), std::stoi(params[8]));
+                        c.radius = std::stoi(params[9]);
+                        circles.push_back(c);
+                    }
+                    else if (params[0] == "Rectangle" && params.size() == 11) {
+                        std::cout << "found rectangle !" << std::endl;
+                        Rectangle r;
+                        r.name = params[1];
+                        r.position.x = std::stof(params[2]);
+                        r.position.y = std::stof(params[3]);
+                        r.speed.x = std::stof(params[4]);
+                        r.speed.y = std::stof(params[5]);
+                        r.color = sf::Color(std::stof(params[6]), std::stof(params[7]), std::stof(params[8]));
+                        r.Size.x = std::stof(params[9]);
+                        r.Size.y = std::stof(params[10]);
+                        rectangles.push_back(r);
                     }
                 }
-                else if (!result[0].compare("Rectangle"))
-                {
-                    std::cout << "Found a Rectangle !" << std::endl;
-                }
-            }
-            else
-            {
-                continue;
+
+                std::cout << "Finished the Line !" << std::endl;
             }
         }
+    }
+
+    ~ConfigFileReader() 
+    {
+        if (inputFile.is_open()) {
+            inputFile.close();
+        }
+        std::cout << "File closed !"<< std::endl;
+    }
+
+    std::vector<std::string> SplitSentence(std::string line) {
+        std::stringstream ss(line);
+        std::string word;
+
+        std::vector<std::string> result;
+
+        while (ss >> word) {
+            result.push_back(word);
+        }
+        
+        return result;
     }
 };
 
+void PrintGlobals(std::string path, sf::Vector2<int> winSize, sf::Font font, std::vector<Circle> circles, std::vector<Rectangle> rectangles) {
+    std::cout << "Path : " << path << std::endl;
+    std::cout << "window height :" << winSize.x << " window width :" << winSize.y << std::endl;
+    std::cout << "Font : " << font.getInfo().family << std::endl;
+    std::cout << "Circles :" << circles.size() << " Rectangles :" << rectangles.size() << std::endl;
+}
+
+void UpdateCircle(sf::CircleShape& drawing, Circle data) {
+    drawing.setRadius(data.radius);
+
+    sf::Vector2f newPosition = drawing.getPosition() + (data.speed * 0.1f);
+    drawing.setPosition(newPosition);
+
+    drawing.setFillColor(data.color);
+}
+void UpdateRectangle(sf::RectangleShape& drawing, Rectangle data) {
+    drawing.setSize(data.Size);
+
+    sf::Vector2f newPosition = drawing.getPosition() + (data.speed * 0.1f);
+    drawing.setPosition(newPosition);
+
+    drawing.setFillColor(data.color);
+}
+
+
 int main()
 {
-    // read the config file
-    ConfigFileReader confReader = ConfigFileReader("D:\\Projects\\2DSimpleGameEngine\\Config.txt");
-    confReader.getWindowConfig();
+    // global variables
+    std::string Path = "D:\\Projects\\2DSimpleGameEngine\\Config.txt";
+    sf::Vector2u windowSize = sf::Vector2(800u, 600u);
+    sf::Font font;
+    // data from the config file
+    std::vector<Circle> CircleData;
+    std::vector<Rectangle> RectangleData;
+
+    // Config file PATH
+    ConfigFileReader(Path, windowSize, font, CircleData, RectangleData);
+
+    // circle shapes to draw
+    std::vector<sf::CircleShape> Circles(CircleData.size());
+    std::vector<sf::RectangleShape> Rectangles(RectangleData.size());
+
 
 
     // create the window
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My window");
+    sf::RenderWindow window(sf::VideoMode({ windowSize.x, windowSize.y }), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -203,15 +178,27 @@ int main()
                 window.close();
         }
         
+        // prepare shapes to be drawn
+        for (int i = 0; i < CircleData.size(); i++) {
+            UpdateCircle(Circles[i], CircleData[i]);
+        }
+        for (int i = 0; i < RectangleData.size(); i++) {
+            UpdateRectangle(Rectangles[i], RectangleData[i]);
+        }
+
+
+        // Drawing process (delete, process , display)
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        window.draw(sf::CircleShape(100.0f));
-        for (auto shape : confReader.getShapes()) {
-            if (shape.getShape() != nullptr) 
-            {
-                window.draw(*shape.getShape());
-            }
+        window.draw(sf::CircleShape(10.0f));
+        for (sf::CircleShape circle : Circles) {
+            window.draw(circle);
+
+        }
+        for (sf::RectangleShape rectangle : Rectangles) {
+            window.draw(rectangle);
+
         }
 
         // end the current frame
